dd1eb58f8300399ea7a8c00dc99354f7944bf075
Austin Kohler <austinckohler@gmail.com>
2020-02-13 17:00:51 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -103,4 +103,29 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  
+  coupCart = cart
+  count = 0
+  while coupons[count] do
+    if cart[coupons[count][:item]]
+      if cart[coupons[count][:item]][:count] >= coupons[count][:num]
+        coupCart[coupons[count][:item]][:count] = coupCart[coupons[count][:item]][:count] - coupons[count][:num]
+        coupItem = "#{coupons[count][:item]} W/COUPON"
+        coupPrice = coupons[count][:cost] / coupons[count][:num]
+        if coupCart[coupItem]
+          coupCount = coupCart[coupItem][:count] + coupons[count][:num]
+        else
+          coupCount = coupons[count][:num]
+        end
+        coupCart[coupItem] = {:price => coupPrice, :clearance => coupCart[coupons[count][:item]][:clearance], :count => coupCount}
+        count += 1
+      else
+        count += 1
+      end
+    else
+      count += 1
+    end
+  end
+  coupCart
+end
+
+
