7ba207b58aed7a50a7cbfcf407fc9b615e325f6c
Austin Kohler <austinckohler@gmail.com>
2020-02-13 15:49:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -74,6 +74,7 @@ end
 def checkout(cart, coupons)
 consolidated_cart =  consolidate_cart(cart) #this consolidates cart 
 couponed_cart = apply_coupons(consolidated_cart) #take consoldated cart AND APPLYS coupons
-clearance_cart = apply_clearance(couponed_cart) #takes couponed and applies clearnace 
+clearance_cart = apply_clearance(couponed_cart) #takes couponedcart  and applies clearnace 
+
 
 end
