aa018f4961cf675bb71907faf192325604c2d813
Austin Kohler <austinckohler@gmail.com>
2020-02-13 17:24:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -89,7 +89,17 @@ end
 # end 
 
 def consolidate_cart(cart)
- 
+   consCart = {}
+  cart.map { |groc|
+    grocItem = groc.keys[0]
+    if consCart[grocItem]
+      consCart[grocItem][:count] += 1
+    else
+      consCart[grocItem] = groc[grocItem]
+      consCart[grocItem][:count] = 1
+    end
+  }
+  consCart
 
 end
 
