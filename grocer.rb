d22dbb98e2347180e7a87925a7499a2a6ac8bb0c
Austin Kohler <austinckohler@gmail.com>
2020-02-13 15:29:16 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -73,6 +73,7 @@ end
 
 def checkout(cart, coupons)
 consolidated_cart =  consolidate_cart(cart)
-couponed_cart = apply_coupons
+couponed_cart = apply_coupons(consolidated_cart)
+clearance_cart = apply_clearance(couponed_cart)
 
 end
