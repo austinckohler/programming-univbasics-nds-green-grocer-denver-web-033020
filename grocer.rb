9fbd52081d24fc77a73584aeda4200b5cf872ae3
Austin Kohler <austinckohler@gmail.com>
2020-02-13 18:00:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -101,3 +101,14 @@ def increment_count_of_item (cart, item_name)
 end
 
 def find_item_by_name_in_collection(name, collection)
+  collection_index = 0
+
+  while collection_index < collection.size do
+    current_item = collection[collection_index]
+    if ( current_item[:item] == name )
+      return current_item
+    end
+    collection_index += 1
+  end
+  nil
+end
